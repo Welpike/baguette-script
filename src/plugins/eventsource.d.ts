3eb6699401ee87f9fffbd8a28d6e1540603edd44
Welpike <pascal.lleonart@gmail.com>
2023-06-20 15:59:34 +0200

    implements buildins _hs plugins

---
+++ src/plugins/eventsource.d.ts
@@ -0,0 +1,28 @@
+///=========================================================================
+/// This module provides the EventSource (SSE) feature for baguette-script
+/// Plugin from _hyperscript
+///=========================================================================
+
+// QUESTION: Is it OK to pack additional data into the "Feature" struct that's returned?
+// TODO: Add methods for EventSourceFeature.connect() and EventSourceFeature.close()
+
+interface EventSourceFeature {
+    name: string
+    object: EventSourceStub
+    install: () => void
+}
+
+interface EventSourceStub {
+    eventSource: EventSource
+    listeners: EventListenerEntry[]
+    retryCount: number
+    open: (url?:string) => void
+    close: () => void
+    addEventListener: (type: keyof HTMLElementEventMap, listener:(event: Event) => any, options?: boolean | AddEventListenerOptions) => void
+}
+
+interface EventListenerEntry {
+    type: string
+    handler: EventHandlerNonNull
+    options?: any
+}
